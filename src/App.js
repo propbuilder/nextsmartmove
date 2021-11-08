6452057bf1fcf761187e8353143e50416aa032e2
amrsingh <amrsingh@lowes.com>
2021-11-08 13:47:23 +0530

    initial set up commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+/**
+ * This web app is authored by Amritansh Singh and usage below libraries for development.
+ * 
+ * --> For reusable UI elements Material UI components.
+ * --> Will be leveraging react testing library and Jest to unit test the components.
+ * --> Will be using cypress for automation tests.
+ * --> Styles will be pure CSS.
+ * --> React routers for routing.
+ * 
+ */
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './components/home';
+import About from './components/aboutMe';
+import NotFound from './components/notFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello there! Welcome to NextSmartMove.
+      <Routes>
+        <Route path="/" element= {<Home />} exact/>
+        <Route path="about" element= {<About />}/>
+        <Route path="/*" element= {<NotFound />}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/notFound.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+const NotFound = () => (
+    <div>
+        <h3>
+            Nothing here :(
+        </h3>
+        <Link to="/">Go To Homepage ^^</Link>
+    </div>
+);
+
+export default NotFound;
